5522566765042267efcd47316354226e27fa7008
solovevaa <solovevaa@mer.ci.nsu.ru>
2022-09-12 13:43:16 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let [value, setValue] = useState(1);
+
+  const clickHandlerPlus = () => {
+    setValue(value + 1);
+  }
+  const clickHandlerMinus = () => {
+    if (value > 0) {
+      setValue(value - 1);
+    }
+  }
+
+  return <>
+    <button onClick={clickHandlerPlus}>+</button>
+      {value}
+    <button onClick={clickHandlerMinus}>-</button>
+    </>
+  ;
 }
 
 export default App;
