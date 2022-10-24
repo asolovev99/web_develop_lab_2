c343ab93dd335a42b98f8547f3e07d3722efd7a0
solovevaa <solovevaa@mer.ci.nsu.ru>
2022-10-24 14:35:02 +0700

    Redo route

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import './App.css';
-import React, {useState} from "react";
-import Timer from "./Components/Timer";
-import TimerForward from "./Components/TimerForward";
-import SelectboxNotRio from "./Components/SelectboxNotRio";
-import Calculator from "./Components/Calculator";
+import React from "react";
 import RegistrationForm from "./Components/RegistrationForm";
 import LoginForm from "./Components/LoginForm";
-
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+    Link
+} from 'react-router-dom';
 import HomeForm from "./Components/HomeForm";
-
-
-
 
 function App() {
     return <>
-        <HomeForm/>
+        <Router>
+            <ul>
+                <li>
+                    <Link to="/">Домашняя страница</Link>
+                </li>
+                <li>
+                    <Link to="/registration">Регистрация</Link>
+                </li>
+                <li>
+                    <Link to="/login">Логин</Link>
+                </li>
+            </ul>
+
+            <Routes>
+                <Route path='/registration' element={<RegistrationForm />} />
+                <Route path='/login' element={<LoginForm />} />
+                <Route path='/' element={<HomeForm/>} />
+            </Routes>
+        </Router>
+
     </>;
 
 }
